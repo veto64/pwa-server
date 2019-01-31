741bd6232378a07fbb2a870dc2e26fcc85221793
veto <veto@debian.dev>
2019-01-31 19:14:19 +0700

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Certificated https web server to serve Progressive Web Apps
+
+The main repository is on:
+[https://calantas.org/pwa-server/](https://calantas.org/pwa-server)
+
+![logo](http://getkudu.io/img/logo.png)
+
+## License
+GNU General Public License v3.0 -
+
+
+
+
---
+++ update_certs.sh
@@ -0,0 +1,6 @@
+curl  http://draconarius.com/html/pdw.calantas.org/pdw.calantas.org_CA.pem --output  etc/ssl/froxlor-custom/pdw.calantas.org_CA.pem
+curl  http://draconarius.com/html/pdw.calantas.org/pdw.calantas.org_chain.pem --output  etc/ssl/froxlor-custom/pdw.calantas.org_chain.pem
+curl  http://draconarius.com/html/pdw.calantas.org/pdw.calantas.org.crt --output  etc/ssl/froxlor-custom/pdw.calantas.org.crt
+curl  http://draconarius.com/html/pdw.calantas.org/pdw.calantas.org_fullchain.pem --output  etc/ssl/froxlor-custom/pdw.calantas.org_fullchain.pem
+curl  http://draconarius.com/html/pdw.calantas.org/pdw.calantas.org.key  --output  etc/ssl/froxlor-custom/pdw.calantas.org.key
+
